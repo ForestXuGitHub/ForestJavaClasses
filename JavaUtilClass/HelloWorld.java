c1763332831a533a92b90abcc60fe11011a4fe37
Forest <fxt98@yahoo.com>
2018-11-06 14:58:24 -0500

    new code changes through develop branch

--- JavaUtilClass/HelloWorld.java
+++ JavaUtilClass/HelloWorld.java
@@ -26,6 +26,9 @@ public class HelloWorld {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
+		
+		// new code added through develop branch
+        System.out.println("Hello, Git");
     }
 
 }
